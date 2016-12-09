93ddb6d56d0c804428e90935dd849a7cedc3f605
Jack <me@jack003.com>
2016-12-09 16:21:09 +0800

    add question

--- exercise/exercise6.py
+++ exercise/exercise6.py
@@ -13,3 +13,17 @@ for i in range(1,100):
 		print i
 	
 
+# good
+
+#def isPrimeNumber(n, s):
+#	for k in s:
+#		if k * k > n: break
+#		if n % k == 0: return None
+#	return n
+#
+#prime = []
+#for n in range(2, 100):
+#	res = isPrimeNumber(n, prime)
+#	if res: prime.append(res)
+#
+#print prime
