c47003e2385e1fc8c5822c2cff8266cbfd897ab4
YUJING1993 <805963294@qq.com>
2016-12-11 11:28:05 +0800

    exercise7_yujing

--- exercise/exercise7.py
+++ exercise/exercise7.py
@@ -5,3 +5,30 @@
 
 # 编写程序，验证指定范围内哥德巴赫猜想的正确性，也就是近似证明哥德巴赫猜想。
 
+x=input('请输入任何一个大于2的偶数： ')
+def isPrimeNumber(n,s):
+	for k in s:
+		if k*k>n:
+			break
+		if n%k==0:
+			return None
+	return n
+	
+prime=[]
+m=0
+for n in range(2,x):
+	res=isPrimeNumber(n,prime)
+	if res:
+	    prime.append(res)
+print prime
+a=len(prime)
+
+for i in range(a/2+2):
+	for j in range(a/2-1,a):
+	    if prime[i]+prime[j]==x:
+		    m+=1
+		    print prime[i],prime[j]
+	if m==1:
+		break
+		
+		  
