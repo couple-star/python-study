b2158c6757c1d3eb216551ad8cd2b3fc183bf5e7
Jack <me@jack003.com>
2016-12-22 22:20:00 +0800

    add question

---
+++ exercise/exercise10.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 实现百度翻译Python SDK
+# http://api.fanyi.baidu.com/api/trans/product/apidoc
+# APP ID: 20161220000034328
+# 密钥: 2UrZfG0h0deQ76TKSH2s
+# reference lib: requests: network request
+#                hashlib: generate md5
+#                random: generate random int
+#                json: transfer json structure to dict
