ab1765dab833392580e43d34e6fb71f7d584a0d1
Jack <me@jack003.com>
2016-12-27 23:45:04 +0800

    add question

--- exercise/exercise10.py
+++ exercise/exercise10.py
@@ -31,3 +31,46 @@ print r.text
 m=json.loads(r.text).get('trans_result')
 print m[0].get('src'), m[0].get('dst')
 
+# good
+
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+# import hashlib
+# import random
+#
+# import requests
+#
+#
+# class Translate:
+#     def __init__(self, sentence, from_lang=None, to_lang=None):
+#         self.appid = '20161220000034328'
+#         self.key = '2UrZfG0h0deQ76TKSH2s'
+#         self.sentence = sentence
+#         self.from_lang = from_lang if from_lang == 'zh' or from_lang == 'en' else 'auto'
+#         self.to_lang = to_lang if to_lang == 'zh' or to_lang == 'en' else 'auto'
+#         self.salt = random.randint(1, 100000)
+#
+#     def query(self):
+#         string = '{0}{1}{2}{3}'.format(self.appid, self.sentence, self.salt, self.key)
+#         self.md5 = hashlib.md5(string).hexdigest()
+#         # print self.md5
+#         base_url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?'
+#         query_url = '{0}q={1}&from={2}&to={3}&appid={4}&salt={5}&sign={6}'.format(
+#             base_url,
+#             self.sentence,
+#             self.from_lang,
+#             self.to_lang,
+#             self.appid,
+#             self.salt,
+#             self.md5
+#         )
+#         r = requests.get(query_url)
+#         return r.json()['trans_result']
+#
+#
+# if __name__ == '__main__':
+#     q_set = '早上好\n晚上好'
+#     sss = Translate(q_set)
+#     r_set = sss.query()
+#     for i, j in enumerate(q_set.split('\n')):
+#         print j, r_set[i]['dst']
--- exercise/exercise11.py
+++ exercise/exercise11.py
@@ -4,28 +4,43 @@
 #API地址是https://developer.github.com/v3/
 
 import requests
-import hashlib
-import random
-import json
+# import json
 
-myurl='https://api.github.com'
-myurl=myurl+'/users/JiaKunUp/repos'
-r=requests.get(myurl)
-print r.text          #打印出里面所有的项目，字符串格式
-m=json.loads(r.text)  #改成Python能识别的模式，此时为list
-lenth=len(m)          #可得知项目的数目为30个
+# myurl='https://api.github.com'
+# myurl=myurl+'/users/JiaKunUp/repos'
+# r=requests.get(myurl)
+# print r.text          #打印出里面所有的项目，字符串格式
+# m=json.loads(r.text)  #改成Python能识别的模式，此时为list
+# lenth=len(m)          #可得知项目的数目为30个
+#
+# j=0
+# star=0
+# for i in range(lenth):
+#     projects=m[i]
+#     stars=projects.get('stargazers_count')
+#     if stars>star:
+#         star=stars
+#         j=i
+#     else:
+#         continue
+# #print star,j
 
-j=0
-star=0
-for i in range(lenth):
-    projects=m[i]
-    stars=projects.get('stargazers_count')
-    if stars>star:
-        star=stars
-        j=i
-    else:
-        continue
-#print star,j
+# print"the most star project is: ",m[17]
+# print"the star number is: ",star
 
-print"the most star project is: ",m[17]
-print"the star number is: ",star
+# excellent
+
+username = raw_input('Please input a username: ')
+
+base_url = 'https://api.github.com'
+query_url = '{0}/users/{1}/repos'.format(base_url, username)
+r = requests.get(query_url)
+result = r.json()
+# repo_list = {x['name']: x['stargazers_count'] for x in result if not x['fork']}
+# sort = sorted(repo_list.items(), key=lambda e: e[1], reverse=True)
+# print sort[0][0], sort[0][1]
+
+
+sort = sorted(result, key=lambda e: e['stargazers_count'], reverse=True)
+# print sort
+print sort[0]['name'], sort[0]['stargazers_count']
