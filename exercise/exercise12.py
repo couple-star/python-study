9caa2b46c0e0ff588d05cbe28af307ab2cc81e6f
YUJING1993 <805963294@qq.com>
2017-01-01 22:59:37 +0800

    update answer

--- exercise/exercise12.py
+++ exercise/exercise12.py
@@ -6,3 +6,25 @@
 # Note:
 # You must do this in-place without making a copy of the array.
 # Minimize the total number of operations.
+
+lists=[3, 2, -5, 6, 4, 0, 9, -7, 0, 1]
+# while True:
+#     num=raw_input('please input the numbers, input q if you want to stop: ')
+#     if num=='q':
+#         break
+#     else:
+#         nums=eval(num)
+#         lists.append(nums)
+#print lists
+# sort=sorted(lists, cmp=lambda x,y : cmp(x, y))
+# print sort
+#
+# for j in range(len(sort)):
+#     i=sort.index(0)
+#     sort.pop(i)
+#     sort.append(0)
+# print sort
+# lists.sort(cmp=lambda x, y: -1 if y == 0 else (x - y) / abs(x - y))
+lists.sort()
+lists.sort(cmp=lambda x, y: -1 if y == 0 else 1)
+print lists
